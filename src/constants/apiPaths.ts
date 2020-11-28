81fb4e697518c372a0ec11e68da42e95c09e6b5b
aleksei-bulgak <aleksei.bulgak@gmail.com>
2020-11-28 15:04:03 +0300

    [refactor] update import url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://kutdlurimk.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://aml75kfrve.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://hsxp3p0ow9.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://kutdlurimk.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
