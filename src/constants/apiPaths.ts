a75420add67997ba3a9d50149e428ac281a1f2a5
aleksei-bulgak <aleksei.bulgak@gmail.com>
2020-12-06 14:23:23 +0300

    [task8] add api gateway proxy for https support

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://hsxp3p0ow9.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://kutdlurimk.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://kutdlurimk.execute-api.eu-west-1.amazonaws.com/dev/cart/api',
+  cart: 'https://q6alx46vvh.execute-api.eu-west-1.amazonaws.com/prod/api',
 };
 
 export default API_PATHS;
