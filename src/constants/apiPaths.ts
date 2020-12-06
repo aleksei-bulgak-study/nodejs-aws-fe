5434055ac7d464ef4a1e8c568b8f5cd92f164309
aleksei-bulgak <aleksei.bulgak@gmail.com>
2020-12-06 11:33:59 +0300

    [task8][feat] add link to beanstalk service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://hsxp3p0ow9.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://kutdlurimk.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://aleksei-bulgak-study-cart-api-prod.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
